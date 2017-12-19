0071d980bcd80e873dadc28e19fb402b89b4f739
vbrazas <appleitunesstore11@gmail.com>
2017-12-19 16:29:55 +0200

    infin_mult 75% done

--- infin_mult/infin_mult.c
+++ infin_mult/infin_mult.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   infin_mult.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbrazas <vbrazas@student.unit.ua>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/19 14:45:24 by vbrazas           #+#    #+#             */
+/*   Updated: 2017/12/19 16:29:18 by vbrazas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "infin_mult.h"
 
@@ -31,7 +42,7 @@ int		ft_strlen(char *buf)
 	return (i);
 }
 
-void	infin_add(char *buf, char *av1, char *av2, int k[])
+char	*infin_add(char *buf, char *av1, char *av2, int k[])
 {
 	int		len;
 	int		rem;
@@ -41,34 +52,38 @@ void	infin_add(char *buf, char *av1, char *av2, int k[])
 	i = ft_strlen(buf);
 	len = i-- + 2;
 	buf = (char *)malloc(sizeof(char) * len);
-	buf[len--] = 0;
-	j = ft_strlen(av2);
+	buf[len--] = '\0';
+	j = ft_strlen(av2) - 1;
 	rem = 0;
 	while (len >= 0)
-	{
-		fn = ((i >= 0 ? av1[i--] : '0') + (j >= 0 ? av2[j--] : '0') +
-		(rem ? rem-- : 0) + '0');
-		if (fn > '9')
+		if ((buf[len--] = ((i >= 0 ? av1[i--] : '0') +
+		(j >= 0 ? av2[j--] : '0') + (rem ? rem-- : 0) - '0')) > '9')
 		{
-			rem = (fn - '0') / 10;
-			buf[len] = (fn - '0') % 10 + '0';
+			rem = 1;
+			buf[len + 1] -= 10;
 		}
-		else if ((buf[len] = fn) <= '9')
-			rem = 0;
-		len--;
-	}
+
+//	free(av1);
+//	free(av2);
+	printf("AAAAAAA buf is : %s\n", buf);
+
 	return (buf);
 }
+
 char	*mult_by_one_digit(char *buf, char *av1, char *av2, int k[])
 {
 	int		len;
 	int		rem;
 	int		fn;
 	int		j;
+	int		nulls;
 
-	len = k[0] - k[3] + 2;
+	nulls = k[5];
+	len = k[0] - k[3] + nulls + 2;
 	buf = (char *)malloc(sizeof(char) * len);
 	buf[len--] = 0;
+	while (nulls--)
+		buf[len--] = '0';
 	j = k[0];
 	rem = 0;
 	while (len >= 0)
@@ -84,12 +99,15 @@ char	*mult_by_one_digit(char *buf, char *av1, char *av2, int k[])
 			rem = 0;
 		len--;
 	}
+	k[5]++;
 	return (buf);
 }
 
-void	infin_mult(char **cop, char *av1, char *av2, int k[])
+int		infin_mult(char **cop, char *av1, char *av2, int k[])
 {
 	int		len;
+	int		len_buf;
+	char	*buf;
 	int		rem;
 
 	len = k[1] - k[4] + 1;
@@ -106,13 +124,18 @@ void	infin_mult(char **cop, char *av1, char *av2, int k[])
 	printf("av2 is : %s\n", av2);
 
 	cop[len--] = NULL;
+	len_buf = len;
 	while (k[1] >= k[4])
 	{
-		cop[len--] = mult_by_one_digit(cop[len], av1, av2, k);
+		cop[len] = mult_by_one_digit(cop[len], av1, av2, k);
+		len--;
 		k[1]--;
 	}
 
+	for (int i = 0; cop[i]; i++)
+		printf("cop[%d] is : %s\n", i, cop[i]);
 	printf("len is : %d\n", len);
+/*
 	len = rem;
 	rem = 0;
 	while (rem <= len)
@@ -120,24 +143,27 @@ void	infin_mult(char **cop, char *av1, char *av2, int k[])
 		printf("cop[%d] is : %p\n", rem, cop[rem]);
 		rem++;
 	}
+*/
+	if ((len = len_buf) == 0)
+		return (printarr(cop[0], k[2]));
+	printf("len is : %d\n", len);
+	infin_add(buf, cop[len], cop[len - 1], k);
+	len -= 2;
+	while (len >= 0)
+		buf = infin_add(NULL, buf, cop[len--], k);
 
-	printarr(cop[0], k[2]);
-
-	rem = 0;
-	while (rem < 20)
-		printf("%d ", cop[0][rem++]);
-
+	return (printarr(buf, k[2]));
 }
 
 int		main(int ac, char **av)
 {
-	int		k[5];
+	int		k[6];
 	int		i;
 
 	if (ac != 3 || !av[1][0] || !av[2][0])
 		return (write(1, "\n", 1));
 	i = 0;
-	while (i < 5)
+	while (i < 6)
 		k[i++] = 0;
 	if (av[1][0] == '-')
 		k[3] = 1;
