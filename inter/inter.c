d951941d1d2a61d1c395b354e7fd95dbfc19f3a1
Millon15 <appleitunesstore11@gmail.com>
2017-12-25 01:52:48 +0200

    aff_z and inter done

---
+++ inter/inter.c
@@ -0,0 +1,76 @@
+
+#include <unistd.h>
+
+void	print(char a[], int b[])
+{
+	int		i;
+
+	i = 0;
+	while (a[i])
+	{
+		if (b[i])
+			write(1, &(a[i]), 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+void	compare(char *av2, char a[], int b[])
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (a[i])
+	{
+		j = 0;
+		while (av2[j] && a[i] != av2[j])
+			j++;
+		if (av2[j])
+			b[i] = 1;
+		i++;
+	}
+}
+
+void	write_a(char *av1, char a[])
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (av1[i])
+	{
+		j = 0;
+		while (a[j] && av1[i] != a[j])
+			j++;
+		if (!a[j])
+		{
+			j = 0;
+			while (a[j])
+				j++;
+			a[j] = av1[i];
+		}
+		i++;
+	}
+}
+
+int		main(int ac, char **av)
+{
+	char	a[27];
+	int		b[27];
+	int		i;
+
+	if (ac != 3)
+		return (write(1, "\n", 1));
+	i = 0;
+	while (i < 27)
+	{
+		a[i] = 0;
+		b[i] = 0;
+		i++;
+	}
+	write_a(av[1], a);
+	compare(av[2], a, b);
+	print(a, b);
+	return (0);
+}
